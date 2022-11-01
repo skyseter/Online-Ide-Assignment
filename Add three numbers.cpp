291aef7f3acd8abc7eecfc9d5704959c5293833a
skyseter <117158791+skyseter@users.noreply.github.com>
2022-11-01 14:43:04 +0530

    Create Add three numbers.cpp

---
+++ Add three numbers.cpp
@@ -0,0 +1,10 @@
+
+/* -------Program to calculate sum of three numbers-------- */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b, c; cin >> a >> b >> c;
+	cout << "The sum of these three numbers is " << a + b + c << "\n";
+}
